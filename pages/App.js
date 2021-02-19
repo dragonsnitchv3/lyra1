d65efdd11e1510d180964b2a6aba5b82942454a1
DragonSnitchX <william.acct002@gmail.com>
2021-02-19 21:23:53 +0800

    chage3

--- pages/App.js
+++ pages/App.js
@@ -16,50 +16,7 @@ class DigitalMarketing extends React.Component {
         script.src = "//js.hs-scripts.com/7851342.js";
         script.async = true;
     
-        document.body.appendChild(script);
-
-        function disableselect(e) {
-            return false
-          }
-          
-          function reEnable() {
-            return true
-          }
-          
-          document.onselectstart = new Function ("return false")
-          
-          if (window.sidebar) {
-            document.onmousedown = disableselect
-            document.onclick = reEnable
-          }
-       
-       
-          document.onkeypress = function (event) {  
-          event = (event || window.event);  
-          if (event.keyCode == 123) {  
-          return false;  
-          }  else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
-                   return false;
-                }
-          }  
-          document.onmousedown = function (event) {  
-          event = (event || window.event);  
-          if (event.keyCode == 123) {  
-          return false;  
-          }  else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
-                   return false;
-                }
-          }  
-          document.onkeydown = function (event) {  
-          event = (event || window.event);  
-          if (event.keyCode == 123) {  
-          return false;  
-          }  else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
-                   return false;
-                }
-          }  
-       
-       document.addEventListener('contextmenu', event => event.preventDefault());
+        
           
       }
 
