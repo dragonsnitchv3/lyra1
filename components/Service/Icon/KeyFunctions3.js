9173e24ff4640ef8c1d41ef9def793534334eba0
DragonSnitchX <william.acct002@gmail.com>
2021-02-25 23:02:50 +0800

    chage3

--- components/Service/Icon/KeyFunctions3.js
+++ components/Service/Icon/KeyFunctions3.js
@@ -3,13 +3,14 @@ import React from 'react';
 const WYWG = ({ nLang, PIcon, PName, PDesc }) => {
 
     return(
-        <div className="col-lg-3 col-md-6 col-12">
+        <div className="col-lg-3 col-6">
             <div className="mb_50 hosting_service_item5a ctr_text">
                 <div className="icon ctr_text2 ">
                     <i className={PIcon}></i>
                 </div>
                 <h1 className="highlight1">{PName}</h1>
-                <p className="ctr_text2">{PDesc}</p>
+               
+                
             </div>
         </div>
     )
--- components/Service/Icon/WYWG2.js
+++ components/Service/Icon/WYWG2.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WYWG = ({ nLang, PIcon, PName, PDesc }) => {
 
     return(
-        <div className="col-lg-3 col-md-6">
+        <div className="col-xl-3 col-lg-4 col-md-6 col-12">
             <div className="hosting_service_item4 ctr_text">
                 <div className="icon">
                     <i className={PIcon}></i>
