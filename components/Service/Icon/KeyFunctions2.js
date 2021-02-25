0798d5f12084db1218fd16898fa151ec9d8b2f31
DragonSnitchX <william.acct002@gmail.com>
2021-02-25 22:18:07 +0800

    chage3

--- components/Service/Icon/KeyFunctions2.js
+++ components/Service/Icon/KeyFunctions2.js
@@ -3,7 +3,7 @@ import React from 'react';
 const KF = ({ nLang, PIcon, PName, PDesc }) => {
 
     return(
-        <div className="col-lg-4">
+        <div className="col-lg-4 col-12">
             <div className="hosting_service_item6 ">
                 <div className="icon ctr_text">
                     <i className={PIcon}></i>
